ddfb802aa1842ecc2a7cbc8654e4b71b25687190
khalid3886 <142919731+khalid3886@users.noreply.github.com>
2024-04-26 18:50:46 +0530

    updated

---
+++ first react app/first project/src/components/Greetings.jsx
@@ -0,0 +1,11 @@
+import styles from './Greetings.module.css'
+function Greetings()
+{
+    return(
+        <>
+        <h1 className={styles.App}>hello greetings from kd.</h1>
+        </>
+    )
+}
+
+export default Greetings
