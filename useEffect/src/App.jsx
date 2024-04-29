2d4920db7aefaacddf5fd939a1717347bd148dad
khalid3886 <142919731+khalid3886@users.noreply.github.com>
2024-04-30 04:19:22 +0530

    a

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { useEffect } from "react"
 import Print from "./conponents/Print"
 function App()
@@ -6,23 +6,27 @@ function App()
   const[data,setData]=useState([])
   const[loading,setLoading]=useState(false)
   const[page,setPage]=useState(1)
+  const[total,setTotal]=useState(0)
 
   const fetchdata=(page)=>{
     setLoading(true)
     fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&&_page=${page}`)
-    .then(res=>res.json())
+    .then(res=>{
+      const totalCount = res.headers.get('X-Total-Count');
+      setTotal(totalCount)
+      return res.json()
+    })
     .then(data=>{
       setData(data)
-      setTimeout(()=>{
         setLoading(false)
-      },0)
     })
     .catch(err=>console.log(err))
   }
 
-  useEffect(()=>{
-    fetchdata()
-  },[])
+useEffect(()=>{
+fetchdata(page)
+},[page])
+ 
 
   if(loading)
   {
@@ -32,11 +36,9 @@ function App()
   return (
     <>
 <div>
-  <button disabled={page==1} onClick={()=>{setPage(page-1)
-  fetchdata(page)}}>prev</button>
+  {page>1 && <button onClick={()=>{setPage(page-1)}}>prev</button>}
   <span>{page}</span>
-  <button disabled={page==11} onClick={()=>{setPage(page+1)
-  fetchdata(page)}}>next</button>
+  {page<(total/10) && <button onClick={()=>{setPage(page+1)}}>next</button>}
   <ul>
     {
       data.map(item=>(
