45ab96e21d0ef0ade150a865f45eb8beb0bfd093
khalid3886 <142919731+khalid3886@users.noreply.github.com>
2024-04-30 01:08:39 +0530

    a

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,24 +1,50 @@
-import { useRef } from "react"
-import { useEffect } from "react"
 import { useState } from "react"
-
+import { useEffect } from "react"
+import Print from "./conponents/Print"
 function App()
 {
-  const a=useRef(0)
-  const[count,setCount]=useState(0)
-  useEffect(()=>{
-    a.current=a.current+1
-    console.log(`value of a is ${a.current}`)
-  })
-console.log(`rendering... ${Date.now()}`)
-  const handleclick=()=>{
-    a.current=a.current+1
-    console.log(`value of a is ${a.current}`)
+  const[data,setData]=useState([])
+  const[loading,setLoading]=useState(false)
+  const[page,setPage]=useState(1)
+
+  const fetchdata=(page)=>{
+    setLoading(true)
+    fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&&_page=${page}`)
+    .then(res=>res.json())
+    .then(data=>{
+      setData(data)
+      setTimeout(()=>{
+        setLoading(false)
+      },0)
+    })
+    .catch(err=>console.log(err))
   }
-  return(
+
+  useEffect(()=>{
+    fetchdata()
+  },[])
+
+  if(loading)
+  {
+    return <h1>loading....</h1>
+  }
+
+  return (
     <>
-    <h1>home page</h1>
-    <button onClick={handleclick}>inc count - {count}</button>
+<div>
+  <button disabled={page==1} onClick={()=>{setPage(page-1)
+  fetchdata(page)}}>prev</button>
+  <span>{page}</span>
+  <button disabled={page==11} onClick={()=>{setPage(page+1)
+  fetchdata(page)}}>next</button>
+  <ul>
+    {
+      data.map(item=>(
+        <Print key={item.id} id={item.id} title={item.title} body={item.body}/>
+      ))
+    }
+  </ul>
+</div>
     </>
   )
 }
---
+++ useEffect/src/conponents/Print.jsx
@@ -0,0 +1,11 @@
+function Print({id,title,body})
+{
+    return(<>
+    <li>
+       <div>title- {title}</div>
+       <div>body- {body}</div>
+        </li>    
+    </>)
+}
+
+export default Print
